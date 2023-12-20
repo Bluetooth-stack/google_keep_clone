7e5fd7789d819f0c0e106bc53416092ae9efa996
Siddharth Hota <siddharthhota26@gmail.com>
2023-12-20 15:39:46 +0530

    added color changing functionality on task input component

--- src/Components/TaskInput/index.js
+++ src/Components/TaskInput/index.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css'
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+// import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-// import FormatColorResetOutlinedIcon from '@mui/icons-material/FormatColorResetOutlined';
+import FormatColorResetOutlinedIcon from '@mui/icons-material/FormatColorResetOutlined';
+import { colors } from '../../Constants';
 
-function TaskInput({openInput, setOpenInput}) {
-   
+function TaskInput({ openInput, setOpenInput, selectedColor, setSelectedColor }) {
+    const [showPalate, setShowPalate] = useState(false)
 
     return (
-        <div className='task-input-holder' onClick={(e)=>{e.stopPropagation()}}>
+        <div
+            className='task-input-holder'
+            style={{ background: selectedColor }}
+            onClick={(e) => { e.stopPropagation(); setShowPalate(false) }}
+        >
             {
                 openInput &&
                 <input type="text" name='title' placeholder='Title' className='title-input' />
             }
             <textarea
-             cols="48" rows="1" 
-             placeholder='Take a note...' 
-             name='content' className='content-input'
-             onClick={()=>{setOpenInput(true)}}
-             >
+                cols="48" rows="1"
+                placeholder='Take a note...'
+                name='content' className='content-input'
+                onClick={() => { setOpenInput(true) }}
+            >
             </textarea>
             {
                 openInput &&
                 <div className='buton-holder'>
                     <div className='edit-palate'>
-                        <ColorLensOutlinedIcon className='palate-icon' />
-                        <DeleteOutlineOutlinedIcon className='palate-icon' />
+                        <ColorLensOutlinedIcon className='palate-icon'
+                            onClick={(e) => { e.stopPropagation(); setShowPalate(!showPalate) }} />
+                        {/* <DeleteOutlineOutlinedIcon className='palate-icon' /> */}
                     </div>
-                    <div role='button' className='create-button' onClick={()=>{setOpenInput(false)}}>Create</div>
+
+                    <div role='button' className='create-button' onClick={() => { setOpenInput(false) }}>Create</div>
+
+                    {
+                        showPalate &&
+                        <div className='color-option-holder'>
+                            <span
+                                className='view-color'
+                                onClick={() => { setSelectedColor('#FFFFFF') }}
+                            >
+                                <FormatColorResetOutlinedIcon className='reset-color-icon' />
+                            </span>
+                            {
+                                colors.map((color, indx) => (
+                                    <span
+                                        key={color + indx}
+                                        style={{ background: color }} className='view-color'
+                                        onClick={() => { setSelectedColor(color) }}
+                                    ></span>
+                                ))
+                            }
+                        </div>
+                    }
+
                 </div>
             }
         </div>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskInput from '../Components/TaskInput'
 
-function Home({openInput, setOpenInput}) {
+function Home({ openInput, setOpenInput }) {
+  const [selectedColor, setSelectedColor] = useState('#FFFFFF')
   return (
     <div className='main-page'>
-      <TaskInput openInput={openInput} setOpenInput={setOpenInput} />
+      <TaskInput openInput={openInput} setOpenInput={setOpenInput} selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
     </div>
   )
 }
